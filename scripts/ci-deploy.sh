9b9e2b03d969cfc77f5b6574e2a9fcab227d145e
Sebastian Ferrari <sebas@taller.net.br>
2017-11-23 17:32:52 -0200

    Added deploy script with docker compose yml.

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -1,5 +1,14 @@
 #!/bin/bash
 
+# Build GitBook
+npm i -g gitbook-cli
+gitbook build
+
+# Deploy
 rsync -avzhe "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
   --exclude '.git' --exclude 'node_modules' --delete --progress ./ \
   taller-docker@162.243.5.100:~/natura-poc.prod/
+
+# Setup Docker services
+ssh -o StrictHostKeyChecking=no taller-docker@162.243.5.100 \
+    'cd /home/taller-docker/natura-poc.prod; docker-compose -f docker-compose.prod.yml up -d'
