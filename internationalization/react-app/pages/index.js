d5590e255657b9eae58116bd79f43fea8aade230
Lucas Constantino Silva <lucasconstantinosilva@gmail.com>
2018-01-26 13:51:53 -0200

    Added DrupalGraphqlBackend with i18next.

--- internationalization/react-app/pages/index.js
+++ internationalization/react-app/pages/index.js
@@ -1,79 +1,143 @@
+/* eslint-disable react/prop-types */
 import React from 'react'
-import PropTypes from 'prop-types'
-import { compose, whithContext, withProps, withHandlers, lifecycle } from 'recompose'
-import { translate } from 'react-i18next'
-import i18n from '../i18n'
-import withData from 'app/lib/withData'
+import { compose } from 'recompose'
+import i18next from 'i18next'
+import { I18nextProvider, translate } from 'react-i18next'
 
-import Container from 'app/components/container'
+import gql from 'graphql-tag'
+import initApollo from '../src/lib/initApollo'
 
-// import Translate, { withTranslate } from '../components/translate'
-// import { withTranslate } from '../components/translate1'
+const apollo = initApollo({})
 
-// const Foo = withTranslate(props => (<pre>{ JSON.stringify(props, null, 2) }</pre>))
+const i18nOptions = {
+  // fallbackLng: 'en',
+  debug: false,
+  whitelist: ['en', 'pt-BR'], // @TODO: list languages from Drupal?
+  ns: [], // do not fetch any namespace unless required.
+  load: 'currentOnly', // avoid 'en' when 'en-US' due to need of fetching.
+}
 
-// const Home = (props) => (
-//   <div>
-//     <h1>Test agregation</h1>
-//     <Translate reset str={ ['teste pai'] }>
-//       <Translate str={ ['teste filho'] }>
-//         <Translate str={ ['teste filho filho'] }>
-//           <Foo />
-//         </Translate>
-//       </Translate>
-//       <Translate str={ ['teste filho2'] }>
-//         <Translate str={ ['teste filho filho2'] }>
-//           <Foo />
-//         </Translate>
-//       </Translate>
-//     </Translate>
-//   </div>
-// )
+class DrupalGraphQLI18nextPlugin {
+  type = '3rdParty'
 
-// const InnerComponent = () => (
-//   <h2><Translate>Inner test</Translate></h2>
-// )
+  queries = {
+    /**
+     * Performs a single key translation fetch.
+     */
+    translation: gql`
+      query Translation($langcode: String!, $string: String!, $context: String! = "") {
+        translation(langcode: $langcode, string: $string, context: $context)
+      }
+    `,
 
-// const Home = (props) => (
-//   <div>
-//     <h1><Translate>Test aggregation</Translate></h1>
-//     <InnerComponent />
-//   </div>
-// )
+    /**
+     * Performs a context (namespace) fetch.
+     */
+    translations: gql`
+      query Translations($language: String!, $context: String! = "") {
+        translations(language: $language, context: $context) {
+          context
+          translation
+          language
+          source
+        }
+      }
+    `
+  }
 
-// const Home = ({ t, translate, locale }) => (
-//   <div>
-//     TESTE
-//     { t({ a: 'teste1' }) }
-//     { t({ b: 'teste2' }) }
-//   </div>
-// )
+  constructor (client) {
+    this.client = client
+  }
 
-const Home = () => (
+  init (i18n) {
+    /**
+     * Set backend plugin.
+     * @TODO: is there any way a i18n plugin can be both 3rdParty and Backend?
+     */
+    i18n.services.backendConnector.backend = {
+      type: 'backend',
+
+      /**
+      * Callback for reading a language on a namespace.
+      */
+      read: (language, context, callback) => {
+        const options = {
+          query: this.queries.translations,
+          variables: { language, context }
+        }
+
+        console.log('reading')
+
+        const query = this.client.query(options)
+
+        // Assign error handling.
+        query.catch(callback)
+
+        // Add result to store.
+        query.then(response => {
+          callback(null, response.data.translations.reduce((result, { source, translation }) => ({
+            ...result,
+            [source]: translation
+          }), {}))
+        })
+      }
+    }
+
+    /**
+     * Listen for missing keys to fetch isolated strings.
+     */
+    i18n.on('*', (e, ...args) => {
+      console.log('EVENt', e, args)
+      // instance.options.parseMissingKeyHandler = () => {
+      //     return options.missingKeyValue;
+      // };
+      //
+      // const languages = typeof lngs === 'string' ? [ lngs ] : lngs;
+      // languages.map(l => requestKey(key, l, ns));
+    })
+  }
+}
+
+/**
+ * Provider HoC.
+ */
+const withI18n = ComposedComponent => class WithI18n extends React.Component {
+  constructor (props) {
+    super(props)
+
+    // Initialize server/client based language-detection solutions.
+    if (process.browser) {
+      i18next.use(require('i18next-browser-languagedetector'))
+    }
+
+    this.i18n = i18next
+      .use(new DrupalGraphQLI18nextPlugin(apollo))
+      .init(i18nOptions)
+  }
+
+  render () {
+    return (
+      <I18nextProvider i18n={ this.i18n }>
+        <ComposedComponent { ...this.props } />
+      </I18nextProvider>
+    )
+  }
+}
+
+const HelloWorld = ({ t = v => v }) => (
   <div>
-    <Container />
+    AQUI: { t('string teste ') }
   </div>
 )
 
-// const Extended = translate(['home'], { i18n, wait: process.browser })(Home)
-//
-// Extended.getInitialProps = async ({ req }) => {
-//   if (req && !process.browser) return i18n.getInitialProps(req, ['home'])
-//   return {}
-// }
+const TranslatedHelloWorld = translate('a')(HelloWorld)
 
-// export default compose(
-//   translate(['home'], { i18n, wait: process.browser }),
-//   // withProps({ 'strArray': [] }),
-//   // withHandlers({
-//   //   locale: ({ strArray }) => e => strArray.push(e)
-//   // }),
-//   // lifecycle({
-//   //   componentDidMount() {
-//   //     this.props.t(this.props.strArray)
-//   //   }
-//   // }),
-// )
-// (Home)
+const Home = () => (
+  <div>
+    <TranslatedHelloWorld />
+  </div>
+)
 
-export default withData(Home)
+export default compose(
+  withI18n
+)(Home)
