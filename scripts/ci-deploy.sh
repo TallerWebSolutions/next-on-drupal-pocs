09093449b55ce6df91a6611415c0000ef0915741
Henrique Recidive <recidive@gmail.com>
2018-01-29 19:05:05 -0200

    Added database to the rsync excluded paths.

--- scripts/ci-deploy.sh
+++ scripts/ci-deploy.sh
@@ -6,7 +6,7 @@ gitbook build
 
 # Deploy
 rsync -avzhe "ssh -o UserKnownHostsFile=/dev/null -o StrictHostKeyChecking=no" \
-  --exclude '.git' --exclude 'node_modules' --delete --progress ./ \
+  --exclude '.git' --exclude 'node_modules' --exclude '.mysql' --delete --progress ./ \
   taller-docker@162.243.5.100:~/natura-poc.prod/
 
 # Setup Docker services
